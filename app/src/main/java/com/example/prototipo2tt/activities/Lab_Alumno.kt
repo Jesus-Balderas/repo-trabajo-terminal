bf4c503988ce56f17d22505f515e82b48abc3a0b
cristiannicolai.hernandez.contractor <cristiannicolai.hernandez.contractor@bbva.com>
2022-03-17 18:19:40 -0600

    Config base ventana laboratorios alumno

---
+++ app/src/main/java/com/example/prototipo2tt/activities/Lab_Alumno.kt
@@ -0,0 +1,13 @@
+package com.example.prototipo2tt.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.prototipo2tt.R
+
+
+class Lab_Alumno : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lab_alumno)
+    }
+}
