d6fc16d688b9add7c67ac438275ec1b32c8a31ad
cristiannicolai.hernandez.contractor <cristiannicolai.hernandez.contractor@bbva.com>
2022-03-25 17:03:26 -0600

    Ventana Laboratorios Alumno - Diseño OK

---
+++ app/src/main/java/com/example/prototipo2tt/activities/ReservaAlumno.java
@@ -0,0 +1,17 @@
+package com.example.prototipo2tt.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.prototipo2tt.R;
+
+
+public class ReservaAlumno extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reserva_alumno);
+    }
+}
